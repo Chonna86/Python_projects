79a7938e9608b462535787e88d7d37eef6f2e85e
Oleksandr Chonka <alexandrchonka@gmail.com>
2023-11-23 17:06:35 +0200

    asd

--- Others/test.py
+++ Others/test.py
@@ -1,18 +1,15 @@
 import aiohttp
 import asyncio
 
-
 async def fetch_data(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             return await response.text()
 
-
 async def main():
-    url = "https://www.google.com"
+    url = 'https://www.google.com'
     data = await fetch_data(url)
     print(data)
-
 
 # Запуск цикла событий asyncio и выполнение асинхронной программы
 asyncio.run(main())
